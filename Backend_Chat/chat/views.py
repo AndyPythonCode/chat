858e56b60ca029de2e441bb062eb3858db03d1de
Andy J. Arciniega <andyarciniegas02@gmail.com>
2021-02-24 09:29:47 -0400

    chat working just missing login

--- Backend_Chat/chat/views.py
+++ Backend_Chat/chat/views.py
@@ -10,7 +10,7 @@ class Room(TemplateView):
     template_name = 'index.html'
 
     #método dispatch mira a la solicitud para determinar si se trata de un GET, POST, etc
-    #Es el punto de entrada para las solicitudes y, en última instancia, el responsable de devolver la respuesta.
+    #Es el punto de entrada para las solicitudes y, en última instancia, el responsable de devolvers la respuesta.
     def dispatch(self, request, **kwargs):
         self.user_name = request.user.username
         return super().dispatch(request, **kwargs)
